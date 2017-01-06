8c3ca3431b7089e04cbe001f1f5a5a0ed32a3ac3
Jacek <jacek@ivolution.pl>
2017-01-06 21:39:46 +0100

    fixing requirements

--- universal_notifications/__init__.py
+++ universal_notifications/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 __title__ = 'Universal Notification'
-__version__ = '0.5.0'
+__version__ = '0.5.1'
 __author__ = 'Pawel Krzyzaniak'
-__license__ = 'Proprietary'
+__license__ = 'MIT'
 __copyright__ = 'Copyright 2016 Arabella'
 
 # Version synonym
