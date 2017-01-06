0582c9e76da11e477f7cfa0cc94c7f0619e572c1
Jacek <jacek@ivolution.pl>
2017-01-06 20:50:00 +0100

    fixing urls.py

--- universal_notifications/api_urls.py
+++ universal_notifications/api_urls.py
@@ -1,15 +1,12 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import patterns
 from django.conf.urls import url
 from rest_framework.routers import DefaultRouter
 
 from universal_notifications.api import DevicesApi
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^devices$', DevicesApi.as_view(), name='notifications_devices_api'),
-
-)
+]
 
 router = DefaultRouter()
 urlpatterns += router.urls
--- universal_notifications/urls.py
+++ universal_notifications/urls.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import url, include, patterns
+from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
 from universal_notifications.docs import (UniversalNotificationsApiView,
                                           UniversalNotificationsResourcesView,
                                           UniversalNotificationsUIView)
 
-urlpatterns = patterns(
+urlpatterns = [
     url(r'^$', UniversalNotificationsUIView.as_view(), name="django.swagger.base.view"),
     url(r'^api-docs/$', UniversalNotificationsResourcesView.as_view(), name="django.swagger.resources.view"),
     url(r'^api-docs/(?P<path>.*)/?$', UniversalNotificationsApiView.as_view(), name='django.swagger.api.view'),
 
     url(r'api/', include('universal_notifications.api_urls')),
-)
+]
 
 router = DefaultRouter()
 urlpatterns += router.urls
