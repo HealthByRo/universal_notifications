aa96dea26fc23eb8255b245e01786886fb773bcf
Jacek <jacek@ivolution.pl>
2017-01-12 12:47:23 +0100

    APD-223 - [BE] implement WS receiver as in doc on Universal Notifications with places to fill

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # universal_notifications
+
+
+To have Universal Notifications receive WS notifications (ie. to mark notification as received) add to your settings.py:
+
+::
+
+    WS4REDIS_SUBSCRIBER = 'universal_notifications.backend.websockets.RedisSubscriber'
+
--- universal_notifications/backends/websockets.py
+++ universal_notifications/backends/websockets.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from rest_framework.renderers import JSONRenderer
 from ws4redis.publisher import RedisPublisher
 from ws4redis.redis_store import RedisMessage
+from ws4redis.subscriber import RedisSubscriber
 
 
 def publish(user, item=None, additional_data=None):
@@ -21,3 +22,16 @@ def publish(user, item=None, additional_data=None):
         # Do not send in tests
         return
     redis_publisher.publish_message(message)
+
+
+class RedisSubscriber(RedisSubscriber):
+    def publish_message(self, message, expire=None):
+        try:
+            # custom code goes here
+            # for efficiency think about using celery to do the job - has to be freaking FAST!
+            print ('message received!')
+            print ('message!')
+        except Exception:
+            # I mean it, catch everything, log it if needed but do catch everything
+            pass
+        return super(RedisSubscriber, self).publish_message(message, expire)
