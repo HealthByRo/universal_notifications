de7237ca55b50bd16d089537c454bd9560efa84e
Michal Proszek <michal.proszek@gmail.com>
2017-01-17 20:25:11 +0100

    APD-182 - [BE] add tests for FCM

--- tests/test_backends.py
+++ tests/test_backends.py
@@ -1,10 +1,14 @@
 import mock
 from django.core import mail
+from django.contrib.auth.models import User
+from django.test.utils import override_settings
 from rest_framework.test import APITestCase
 from rest_framework.renderers import JSONRenderer
 
 from universal_notifications.backends.emails import send_email
 from universal_notifications.backends.websockets import publish
+from universal_notifications.backends.push.fcm import fcm_send_message
+from universal_notifications.models import Device
 
 
 class SampleUser(object):
@@ -64,3 +68,33 @@ class EmailTests(APITestCase):
             self.assertEqual(sent_email.subject, sample_email['subject'])
             self.assertEqual(sent_email.to, [sample_email['to']])
             self.assertIn(sample_email['message'], sent_email.body)
+
+
+class PushTests(APITestCase):
+    test_settings = {
+        'app1': {
+            'FCM_API_KEY': 'secret'
+        }
+    }
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='user', email='user@example.com', password='1234')
+
+        self.fcm_device = Device(user=self.user, app_id='app1', platform=Device.PLATFORM_FCM)
+
+    @override_settings(UNIVERSAL_NOTIFICATIONS_MOBILE_APPS=test_settings)
+    def test_fcm(self):
+        with mock.patch('universal_notifications.backends.push.fcm.FCMNotification.'
+                        'notify_single_device') as mocked_notify:
+            message = {'device': self.fcm_device, 'message': 'msg', 'data': {'stuff': 'foo'}}
+
+            with override_settings(UNIVERSAL_NOTIFICATIONS_MOBILE_APPS={'app1': {}}):
+                fcm_send_message(**message)
+                mocked_notify.assert_not_called()
+
+            mocked_notify.reset_mock()
+
+            fcm_send_message(**message)
+            mocked_notify.assert_called_with(registration_id=message['device'].notification_token,
+                                             message_body=message['message'], data_message=message['data'])
