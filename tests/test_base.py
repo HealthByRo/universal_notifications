9eb173b429f02456ca4432a78912e0fd40293c4d
Michal Proszek <michal.proszek@gmail.com>
2017-03-19 21:12:49 +0100

    APD-182 - [BE] add missing tests for Notification.send_inner

--- tests/test_base.py
+++ tests/test_base.py
@@ -16,7 +16,7 @@ from django.contrib.auth.models import User
 from django.db import models
 from rest_framework import serializers
 from rest_framework.test import APITestCase
-from universal_notifications.models import UnsubscribedUser
+from universal_notifications.models import Device, UnsubscribedUser
 from universal_notifications.notifications import (EmailNotification,
                                                    NotificationBase,
                                                    PushNotification,
@@ -155,6 +155,11 @@ class BaseTest(APITestCase):
         self.object_receiver = SampleReceiver('foo@bar.com', '123456789')
         self.superuser_object_receiver = SampleReceiver('super_foo@bar.com', '123456789', is_superuser=True)
 
+        self.regular_user = User.objects.create_user(
+            username='barszcz',
+            email='bar@sz.cz',
+            password='1234'
+        )
         self.all_unsubscribed_receiver = User.objects.create_user(
             username='all_unsubscribed_user',
             email='bar@foo.com',
@@ -175,13 +180,10 @@ class BaseTest(APITestCase):
             unsubscribed={"email": ["default"]}
         )
 
-    def test_sending(self):
-        """ sending
-            - test sending
+        self.push_device = Device.objects.create(
+            user=self.regular_user, platform=Device.PLATFORM_FCM)
 
-            TODO (Pawel):
-                - test receiver list preparation
-                - test message serialization """
+    def test_sending(self):
         with mock.patch('tests.test_base.SampleA.send_inner') as mocked_send_inner:
             mocked_send_inner.return_value = None
 
@@ -198,17 +200,28 @@ class BaseTest(APITestCase):
         # test WSNotifications
         with mock.patch('tests.test_base.SampleD.send_inner') as mocked_send_inner:
             SampleD(self.object_item, [self.object_receiver], {}).send()
-            mocked_send_inner.assert_called_with({self.object_receiver}, {
+            expected_message = {
                 'message': SampleD.message,
                 'data': {
                     'name': self.object_item.name
                 }
-            })
+            }
+            mocked_send_inner.assert_called_with({self.object_receiver}, expected_message)
+
+        # test send_inner
+        with mock.patch('universal_notifications.notifications.publish') as mocked_publish:
+            SampleD(self.object_item, [self.object_receiver], {}).send()
+            mocked_publish.assert_called_with(self.object_receiver, additional_data=expected_message)
 
         # test SMSNotifications
         with mock.patch('tests.test_base.SampleE.send_inner') as mocked_send_inner:
             SampleE(self.object_item, [self.object_receiver], {}).send()
             mocked_send_inner.assert_called_with({self.object_receiver.phone}, self.object_item.name)
+
+        # test send_inner
+        with mock.patch('universal_notifications.notifications.send_sms') as mocked_send_sms:
+            SampleE(self.object_item, [self.object_receiver], {}).send()
+            mocked_send_sms.assert_called_with(self.object_receiver.phone, self.object_item.name)
 
         # test EmailNotifications
         with mock.patch('tests.test_base.SampleF.send_inner') as mocked_send_inner:
@@ -250,17 +263,23 @@ class BaseTest(APITestCase):
         # test PushNotifications
         with mock.patch('tests.test_base.SampleG.send_inner') as mocked_send_inner:
             SampleG(self.object_item, [self.object_receiver], {}).send()
-            mocked_send_inner.assert_called_with({self.object_receiver}, {
+            expected_message = {
                 'message': self.object_item.name,
                 'data': {}
-            })
+            }
+            mocked_send_inner.assert_called_with({self.object_receiver}, expected_message)
+
+        # test send_inner
+        with mock.patch('tests.test_base.Device.send_message') as mocked_send_message:
+            SampleG(self.object_item, [self.regular_user], {'item': self.object_item}).send()
+            mocked_send_message.assert_called_with(self.object_item.name)
 
         # test w/o category - should fail
         with mock.patch('tests.test_base.SampleNoCategory.send_inner') as mocked_send_inner:
             with self.assertRaises(ImproperlyConfigured):
                 SampleNoCategory(self.object_item, [self.object_receiver, self.unsubscribed_receiver], {}).send()
 
-        # chat catgory is not allowed for "user"
+        # chat category is not allowed for "user"
         with self.assertRaises(ImproperlyConfigured):
             SampleChatNotification(self.object_item, [self.object_receiver], {}).send()
 
