d46eb62393fbe6d9cdbe279a4ba4814a66a21080
Remigiusz Dymecki <remigiusz@gmail.com>
2017-04-05 12:14:10 +0200

    Support for Django 1.11 and Twilio 6.x

--- universal_notifications/backends/sms/engines/twilio.py
+++ universal_notifications/backends/sms/engines/twilio.py
@@ -2,8 +2,9 @@ from __future__ import absolute_import
 from django.conf import settings
 from redis import StrictRedis
 from rest_framework.renderers import JSONRenderer
-from twilio.rest import TwilioLookupsClient, TwilioRestClient
-from twilio.rest.exceptions import TwilioRestException
+from twilio.rest.lookups import Lookups
+from twilio.rest import Client
+from twilio.base.exceptions import TwilioException
 from universal_notifications.backends.sms.abstract import SMSEngineAbtract
 from universal_notifications.backends.sms.utils import report_admins
 from universal_notifications.models import Phone, PhoneReceived, PhoneReceivedRaw, PhoneReceiver, PhoneSent
@@ -15,10 +16,10 @@ import phonenumbers
 
 def get_twilio_client(lookups=False):
     if lookups:
-        return TwilioLookupsClient(settings.UNIVERSAL_NOTIFICATIONS_TWILIO_ACCOUNT,
-                                   settings.UNIVERSAL_NOTIFICATIONS_TWILIO_TOKEN)
-    return TwilioRestClient(settings.UNIVERSAL_NOTIFICATIONS_TWILIO_ACCOUNT,
-                            settings.UNIVERSAL_NOTIFICATIONS_TWILIO_TOKEN)
+        return Lookups(settings.UNIVERSAL_NOTIFICATIONS_TWILIO_ACCOUNT,
+                       settings.UNIVERSAL_NOTIFICATIONS_TWILIO_TOKEN)
+    return Client(settings.UNIVERSAL_NOTIFICATIONS_TWILIO_ACCOUNT,
+                  settings.UNIVERSAL_NOTIFICATIONS_TWILIO_TOKEN)
 
 
 class Engine(SMSEngineAbtract):
@@ -65,7 +66,7 @@ class Engine(SMSEngineAbtract):
                         params['media_url'] = "%s%s" % (settings.MEDIA_URL, obj.media_raw)
                 message = twilio_client.messages.create(**params)
                 obj.sms_id = message.sid
-            except TwilioRestException as e:
+            except TwilioException as e:
                 obj.error_message = e
                 obj.status = PhoneSent.STATUS_FAILED
 
