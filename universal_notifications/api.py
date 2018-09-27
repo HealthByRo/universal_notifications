f7b5b7718f2ce7c6878aae93144fd3d863e708a8
Michal Proszek <michal.proszek@gmail.com>
2018-09-27 16:39:35 -0400

    [BE] allow deleting devices

--- universal_notifications/api.py
+++ universal_notifications/api.py
@@ -1,13 +1,21 @@
 # -*- coding: utf-8 -*-
-from rest_framework.generics import CreateAPIView, GenericAPIView
+from rest_framework.generics import CreateAPIView, DestroyAPIView, GenericAPIView
 from rest_framework.mixins import RetrieveModelMixin, UpdateModelMixin
 from rest_framework.permissions import IsAuthenticated
-from universal_notifications.models import UnsubscribedUser
+from universal_notifications.models import Device, UnsubscribedUser
 from universal_notifications.serializers import DeviceSerializer, UnsubscribedSerializer
 
 
 class DevicesAPI(CreateAPIView):
     serializer_class = DeviceSerializer
+
+
+class DeviceDetailsAPI(DestroyAPIView):
+    queryset = Device.objects.all()
+    serializer_class = DeviceSerializer
+
+    def get_queryset(self):
+        return super().get_queryset().filter(user=self.request.user)
 
 
 class SubscriptionsAPI(RetrieveModelMixin, UpdateModelMixin, GenericAPIView):  # we don't want patch here
