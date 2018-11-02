1e2638aafa5cc6ea0ef884263e529312e9507e7a
Michal Proszek <michal.proszek@gmail.com>
2018-11-02 16:05:27 +0100

    Upgrade swagger.

--- universal_notifications/api_urls.py
+++ universal_notifications/api_urls.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url
 from rest_framework.routers import DefaultRouter
-from universal_notifications.api import DeviceDetailsAPI, DevicesAPI, SubscriptionsAPI
+from universal_notifications.api import DeviceDetailsAPI, DevicesAPI, SubscriptionsAPI, UniversalNotificationsDocsView
 from universal_notifications.backends.twilio.api import TwilioAPI
 
 urlpatterns = [
     url(r"^devices$", DevicesAPI.as_view(), name="notifications-devices"),
     url(r"^devices/(?P<pk>[\d]+)$", DeviceDetailsAPI.as_view(), name="device-details"),
     url(r"^twilio$", TwilioAPI.as_view(), name="twilio-callback"),
-    url(r"^subscriptions$", SubscriptionsAPI.as_view(), name="notifications-subscriptions")
+    url(r"^subscriptions$", SubscriptionsAPI.as_view(), name="notifications-subscriptions"),
+    url(r"^api-docs/", UniversalNotificationsDocsView.as_view(), name="notifications-docs")
 ]
 
 router = DefaultRouter()
--- universal_notifications/docs.py
+++ universal_notifications/docs.py
@@ -1,22 +1,10 @@
 # -*- coding: utf-8 -*-
-import json
 from importlib import import_module
 
-import rest_framework_swagger as rfs
 from django.apps import apps
-from django.conf import settings
-from django.core.exceptions import PermissionDenied
-from django.shortcuts import render_to_response
-from django.template import RequestContext, TemplateDoesNotExist, TemplateSyntaxError
-from django.utils import six
+from django.template import TemplateDoesNotExist, TemplateSyntaxError
 from django.utils.safestring import mark_safe
-from django.views.generic import View
-from rest_framework.settings import api_settings
-from rest_framework.views import Response
-from rest_framework_swagger.apidocview import APIDocView
-from rest_framework_swagger.compat import OrderedDict, import_string
-from rest_framework_swagger.docgenerator import DocumentationGenerator
-from rest_framework_swagger.introspectors import IntrospectorHelper
+from rest_framework.compat import coreapi
 from universal_notifications.notifications import (EmailNotification, NotificationBase, PushNotification,
                                                    SMSNotification, WSNotification)
 
@@ -35,7 +23,7 @@ class BaseGenerator(object):
         return "TODO: type"
 
     def get_notes(self):
-        parts = [self._obj.__doc__]
+        parts = [self.get_summary() + "\n", self._obj.__doc__]
         if self._obj.check_subscription:
             parts.append("<b>Subscription Category:</b> %s" % self._obj.category)
         parts.append(self.get_class_specific_notes())
@@ -129,11 +117,6 @@ class EmailDocGenerator(BaseGenerator):
         return not self._obj.email_name
 
 
-class PatchedDocumentationGenerator(DocumentationGenerator):
-    def __init__(self, *args, **kwargs):
-        pass  # ignore init from oryginal documentation generator
-
-
 class NotificationsDocs(object):
     _registry = {}
     _autodiscovered = False
@@ -143,7 +126,6 @@ class NotificationsDocs(object):
         SMSNotification: SMSDocGenerator,
         WSNotification: WSDocGenerator
     }
-    _pdg = PatchedDocumentationGenerator()
 
     @classmethod
     def autodiscover(cls):
@@ -176,9 +158,6 @@ class NotificationsDocs(object):
             except Exception:
                 pass
 
-        # serializers
-        cls._serializers.update(cls._pdg._find_field_serializers(cls._serializers))
-
     @classmethod
     def get_types(cls):
         return sorted(cls._registry.keys())
@@ -196,159 +175,14 @@ class NotificationsDocs(object):
 
     @classmethod
     def generate_notifications_docs(cls, notification_type):
-        notifications_docs = []
+        links = {}
         for key, value in cls.get_notifications(notification_type):
             generator = cls.get_generator(value["cls"])
-            notifications_docs.append({
-                "operations": [{
-                    "parameters": [],
-                    "nickname": key,
-                    "notes": generator.get_notes(),
-                    "summary": generator.get_summary(),
-                    "type": generator.get_type(),
-                    "method": "GET"
-                }],
-                "path": value["path"],
-                "description": ""
-            })
+            links[value["path"]] = coreapi.Link(
+                title=key,
+                description=generator.get_notes(),
+                action="GET",
+                url=value["path"]
+            )
 
-        return notifications_docs
-
-    # ++ CODE TAKEN FROM DJANGO REST SWAGGER & MODIFIED
-
-    @classmethod
-    def generate_models_docs(cls):
-        models_docs = {}
-
-        for serializer in cls._serializers:
-            data = cls._pdg._get_serializer_fields(serializer)
-
-            serializer_name = IntrospectorHelper.get_serializer_name(serializer)
-            r_name = serializer_name
-            r_properties = OrderedDict((k, v) for k, v in data["fields"].items() if k not in data["write_only"])
-            models_docs[r_name] = {
-                "id": r_name,
-                "required": [i for i in r_properties.keys()],
-                "properties": r_properties,
-            }
-
-        return models_docs
-    # -- CODE TAKEN FROM DJANGO REST SWAGGER & MODIFIED
-
-
-# ++ CODE TAKEN FROM DJANGO REST SWAGGER & MODIFIED
-
-try:
-    JSONRenderer = list(filter(
-        lambda item: item.format == "json",
-        api_settings.DEFAULT_RENDERER_CLASSES,
-    ))[0]
-except IndexError:
-    from rest_framework.renderers import JSONRenderer
-
-
-def get_full_base_path(request):
-    try:
-        base_path = rfs.SWAGGER_SETTINGS["base_path"]
-    except KeyError:
-        return request.build_absolute_uri(request.path).rstrip("/")
-    else:
-        protocol = "https" if request.is_secure() else "http"
-        return "{0}://{1}".format(protocol, base_path.rstrip("/"))
-
-
-class UniversalNotificationsUIView(View):
-    def get(self, request, *args, **kwargs):
-        if not self.has_permission(request):
-            return self.handle_permission_denied(request)
-
-        template_name = rfs.SWAGGER_SETTINGS.get("template_path")
-        data = {
-            "swagger_settings": {
-                "discovery_url": "%s/api-docs/" % get_full_base_path(request),
-                "api_key": rfs.SWAGGER_SETTINGS.get("api_key", ""),
-                "api_version": rfs.SWAGGER_SETTINGS.get("api_version", ""),
-                "token_type": rfs.SWAGGER_SETTINGS.get("token_type"),
-                "enabled_methods": [],
-                "doc_expansion": rfs.SWAGGER_SETTINGS.get("doc_expansion", ""),
-            },
-            "django_settings": {
-                "CSRF_COOKIE_NAME": mark_safe(
-                    json.dumps(getattr(settings, "CSRF_COOKIE_NAME", "csrftoken"))),
-            }
-        }
-        try:
-            return render_to_response(template_name, RequestContext(request, data))
-        except TypeError:
-            # fix for a bug in django 1.11
-            return render_to_response(template_name, data)
-
-    def has_permission(self, request):
-        if rfs.SWAGGER_SETTINGS.get("is_superuser") and not request.user.is_superuser:
-            return False
-
-        if rfs.SWAGGER_SETTINGS.get("is_authenticated") and not request.user.is_authenticated():
-            return False
-
-        return True
-
-    def handle_permission_denied(self, request):
-        permission_denied_handler = rfs.SWAGGER_SETTINGS.get("permission_denied_handler")
-        if isinstance(permission_denied_handler, six.string_types):
-            permission_denied_handler = import_string(permission_denied_handler)
-
-        if permission_denied_handler:
-            return permission_denied_handler(request)
-        else:
-            raise PermissionDenied()
-
-
-class UniversalNotificationsResourcesView(APIDocView):
-    renderer_classes = (JSONRenderer, )
-
-    def get(self, request, *args, **kwargs):
-        apis = [{"path": "/" + path} for path in self.get_resources()]
-        return Response({
-            "apiVersion": rfs.SWAGGER_SETTINGS.get("api_version", ""),
-            "swaggerVersion": "1.2",
-            "basePath": self.get_base_path(),
-            "apis": apis,
-            "info": rfs.SWAGGER_SETTINGS.get("info", {
-                "contact": "",
-                "description": "",
-                "license": "",
-                "licenseUrl": "",
-                "termsOfServiceUrl": "",
-                "title": "",
-            }),
-        })
-
-    def get_base_path(self):
-        try:
-            base_path = rfs.SWAGGER_SETTINGS["base_path"]
-        except KeyError:
-            return self.request.build_absolute_uri(
-                self.request.path).rstrip("/")
-        else:
-            protocol = "https" if self.request.is_secure() else "http"
-            return "{0}://{1}/{2}".format(protocol, base_path, "api-docs")
-
-    def get_resources(self):
-        return NotificationsDocs.get_types()
-
-
-class UniversalNotificationsApiView(APIDocView):
-    renderer_classes = (JSONRenderer, )
-
-    def get(self, request, path, *args, **kwargs):
-        result = Response({
-            "apiVersion": rfs.SWAGGER_SETTINGS.get("api_version", ""),
-            "swaggerVersion": "1.2",
-            "basePath": self.api_full_uri.rstrip("/"),
-            "resourcePath": "/" + path,
-            "apis": NotificationsDocs.generate_notifications_docs(path),
-            "models": NotificationsDocs.generate_models_docs(),
-        })
-
-        return result
-# -- CODE TAKEN FROM DJANGO REST SWAGGER & MODIFIED
+        return links
