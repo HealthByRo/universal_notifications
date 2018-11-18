cba05212415bc7ce0988bf515346aad8b86394b7
Remigiusz Dymecki <remigiusz@gmail.com>
2018-11-18 13:32:53 +0100

    Change owner and drop old python

--- universal_notifications/__init__.py
+++ universal_notifications/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 __title__ = "Universal Notifications"
-__version__ = "1.1.0"
+__version__ = "1.2.0"
 __author__ = "Pawel Krzyzaniak"
 __license__ = "MIT"
-__copyright__ = "Copyright 2017 Arabella"
+__copyright__ = "Copyright 20171-2018 Arabella; 2018+ Ro"
 
 # Version synonym
 VERSION = __version__
