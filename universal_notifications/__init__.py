d1bad5e10c7ac698870d3127a4bafd2f1b748299
Remigiusz Dymecki <remigiusz@gmail.com>
2018-11-18 13:50:06 +0100

    fix "coreapi" requirements

--- universal_notifications/__init__.py
+++ universal_notifications/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 __title__ = "Universal Notifications"
-__version__ = "1.2.0"
+__version__ = "1.2.1"
 __author__ = "Pawel Krzyzaniak"
 __license__ = "MIT"
 __copyright__ = "Copyright 20171-2018 Arabella; 2018+ Ro"
